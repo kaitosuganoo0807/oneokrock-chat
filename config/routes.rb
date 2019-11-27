485e0bcc80d4391dcd7c583018e2b4eed18c83e5
kaitosuganoo0807 <k.0807roadster@gmail.com>
2019-11-28 02:11:34 +0900

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :tweets, only: [:index, :new]
+  resources :tweets, only: [:index, :new, :create]
   resources :users, only: [:edit, :update]
   get 'posts/music', to: 'posts#music'
 end
